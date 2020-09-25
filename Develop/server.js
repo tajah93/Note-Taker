c66f79e37adce9c7cf03f947914e623f8fd94883
Tajah Johnson <tajah93@hotmail.com>
2020-09-24 22:32:18 -0500

    Created util.promisify of readFile and writeFile

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,14 @@
 var fs = require("fs");
+var util = require("util");
 var express = require("express");
 var path = require("path"); 
 
 var app = express(); 
 
 var PORT = process.env.PORT || 8080;
+
+var readFile = util.promisify(fs.readFile);
+var writeFile= util.promisify(fs.writeFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +18,7 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/assets/notes.html"), function (err) {
           console.log('Sent:')
       });
-      fs.readFile("./develop/db/db.json", "utf8"
+      readFile("./db/db.json", "utf8"
       ).then((data) => {
         var myNotes = [].concat(JSON.parse(data));
         res.json(myNotes);
@@ -30,23 +34,23 @@ app.post("/api/notes", function(req, res) {
     myNotes.push(newNote);
     return myNotes 
   }).then((myNotes) => {
-    fs.writeFile("./develop/db/db.json", JSON.stringify(myNotes))
+    writeFile("./develop/db/db.json", JSON.stringify(myNotes))
     res.json(newNote);
   })
 )});
  
-app.delete("/api/notes", function(req, res) {
-  fs.readFile("./develop/db/db.json", "utf8"
-  ).then ((data) => {
-    var myNotes = [].concat(JSON.parse(data));
-    var info = [];
-    for (let n = 0; myNotes.length; n++) {
-      if( === myNotes[n].id)
-    }
+// app.delete("/api/notes", function(req, res) {
+//   fs.readFile("./develop/db/db.json", "utf8"
+//   ).then ((data) => {
+//     var myNotes = [].concat(JSON.parse(data));
+//     var info = [];
+//     for (let n = 0; myNotes.length; n++) {
+//       if( === myNotes[n].id)
+//     }
 
-  }
-}
-)
+//   }
+// }
+// )
 
 app.get("/", function (req, res){
   res.sendFile(path.join(_dirname, "./public/assets/index.html"))
